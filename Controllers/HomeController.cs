2c29a50c81e7e3234487633e3e4a160cbb1b26fa
Gunel <gunelfh@code.edu.az>
2022-06-02 05:25:45 +0400

    First commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace First_practice.Controllers
+{
+    public class HomeController:Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult About()
+        {
+            ViewResult view = new ViewResult();
+
+            view.ViewName = "About";
+            return view;
+        }
+        public ActionResult Faq()
+        {
+            return View();
+        }
+    }
+}
